05282816df81f7b6083ec3d4a995724e45fa50ba
Leonardo <leonardoraupprodrigues@hotmail.com>
2023-12-21 20:51:49 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import Primeiro from './components/Primeiro';
+
+function App() {
+  return (
+    <View>
+      <Primeiro />
+    </View>
+  );
+}
+export default App;
